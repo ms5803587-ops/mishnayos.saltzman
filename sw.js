c1a23742469fb136b7f6227efbce180cc48ec541
ms5803587-ops <ms5803587@gmail.com>
2026-05-18 02:37:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "mishnayot-zaltsman-v21-structure-fixed";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./logo.png",
+  "./header-logo.png",
+  "./site-icon-round.png",
+  "./home-bg.png",
+  "./home-bg-mobile.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS).catch(()=>{})));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
